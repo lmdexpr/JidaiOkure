ad907027ad6d2a0db333e8a458d1cfd4c2d68c6a
lmdexpr <lmdexpr@gmail.com>
2015-01-20 08:04:33 +0900

    modify: kireru syntax

--- JidaiOkure.rb
+++ JidaiOkure.rb
@@ -136,7 +136,8 @@ end
 class Kireru < JidaiNoOkure
   class << self
     def target
-      /^(#{@@my_name}|@?#{@@screen_name})が(何|なに)を(言|い)ったところで.+(という|っていう)(感|かん)じだ$/u
+      /^(#{@@my_name}|@?#{@@screen_name})が(何|なに)を(言|い)ったところで.+
+      (といった|という|っていう)(感|かん)じだ$/xu
     end
 
     def run(str, rep_id, rep_sn)
